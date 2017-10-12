6dbcd07799bc65f61cc297a34ff5c0a5326c2ebd
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-12 15:57:59 -0500

    initial commit

---
+++ lootbag.rb
@@ -0,0 +1,8 @@
+
+class Bag
+
+	# def initialize
+
+	# end
+
+end
---
+++ test_lootbag.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative 'lootbag'
+
+class LootTest < MiniTest::Test
+
+	def setup
+		puts "Setup Initialized"
+	end
+
+	def teardown
+		puts "Tearing Down"
+	end
+
+end
